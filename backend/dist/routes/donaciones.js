63d7f355070b85ad9c7fba04e888915bd2927402
MarSGlez <martin.sanchezg@hotmail.com>
2025-10-16 20:54:51 -0600

    cambios donaciones

--- backend/dist/routes/donaciones.js
+++ backend/dist/routes/donaciones.js
@@ -5,5 +5,5 @@ const donaciones_1 = require("../controllers/donaciones");
 const router = (0, express_1.Router)();
 router.post("/api/donacion/savedonacion/", donaciones_1.saveDonacion);
 router.get("/api/donacion/getdonacion/:rfc", donaciones_1.getDonacion);
-router.get("/api/donacion/validate/:rfc", donaciones_1.validateToken);
+router.post("/api/donacion/validate/", donaciones_1.validateToken);
 exports.default = router;
